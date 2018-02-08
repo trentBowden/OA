af7649d02aa0b6fc0e26a04d478f62abf92e7284
trent bowden <tbowden@atlassian.com>
2018-02-08 20:19:01 +1100

    started

--- public/database_get.js
+++ public/database_get.js
@@ -1,7 +1,8 @@
 var mostRecent = document.getElementById('roomCode');
 var phoneAnswer = document.getElementById('phoneAnswer');
 var connectedStatusDiv = document.getElementById('connectedStatus');
-var randomString = Math.random().toString(36).substring(7);
+var randomString = Math.random().toString(36).substring(10);
+//substring(7) before, but doubt I'll need that.
 mostRecent.innerText = randomString;
 var changedTo;
 var ref = firebase.database().ref('current/' + randomString);
--- public/scripts_new.js
+++ public/scripts_new.js
@@ -1,41 +1,81 @@
-//Variables
-var startingLevel = 300;
-var currentLevel = startingLevel;
-var sizeDifference = 30; // Size diff between levels, in nextLevelCheck()
+//Media Variables
 var srces = ["images/up.png", "images/down.png", "images/left.png", "images/rightNew.png"];
 var audio = new Audio('sounds/success.wav');
 var click = new Audio('sounds/click.mp3');
+//Vocabulary variables
 var humanWords = ["up", "down", "left", "right"];
+//Score
+var totalScore = 0;
+//Current elements
 var currentLine = [];
 var currentHumanWord;
 var currentRand;
 var currentInspecting = 0;
-var totalScore = 0;
+//Size
+var startingSize = 300;
+var currentSize = startingSize;
+var sizeDifference = 30; // Size diff between levels, in nextLevelCheck()
 
-
+//Measurements in metres
+var distanceFromChart = 6;
+var measurementList = [61,30.5,21.3,15.2,12.2,9.14, 7.62, 6.10];
+var sizeForLevels = [];
+var currentLevel = 0;
 //Functions
-function partOfVocabulary(input) {
-    if ((input == "left") ||
-        (input == "right") ||
-        (input == "up") ||
-        (input == "down")) {
-        return true;
-    } else {
-        return false;
-    }
+
+$("#select_dialect").hide();
+
+
+function letterSize(dist) {
+    var letterSizeMeters = (tanDegrees(5/60))*dist;
+    var letterSizeCM = letterSizeMeters * 100;
+    return letterSizeCM;
 }
+
+function tanDegrees(angle) {
+    return Math.tan(angle/180*Math.PI);
+};
+
+for(var i = 0; i<measurementList.length; i++) {
+    sizeForLevels.push(letterSize(measurementList[i]));
+    console.log(sizeForLevels[i]);
+}
+
+
+function partOfVocabulary(input) {
+
+    var english = ["left", "right", "up", "down"];
+    var japanese = ["右","左","上","下"];
+    for (var i = 0; i<english.length; i++) {
+        var regX = new RegExp(english[i].toUpperCase());
+        if (regX.test(input.trim().toUpperCase())) {
+            console.log("This word is " + english[i]);
+            return english[i];
+        }
+    }
+    for (var i = 0; i<japanese.length; i++) {
+        var regX = new RegExp(japanese[i].toUpperCase());
+        if (regX.test(input.trim().toUpperCase())) {
+            console.log("This word is " + japanese[i]);
+            return english[i];
+        }
+    }
+    //add any other for loops for other languages
+    return false;
+}
+
 function displayToUser(input) {
     console.log("Found it! you said "+ input );
-    latestAnswer.innerHTML = input;
+    // latestAnswer.innerHTML = input;
 }
 function correctLetterCheck(input){
-    var currentLevelPoints = 500-currentLevel;
+    var currentLevelPoints = 500-currentSize;
     if (input == currentLine[currentInspecting]) {
         totalScore+=currentLevelPoints;
-        score.innerHTML=currentLevelPoints;
-        latestAnswer.style.borderBottom = "thick solid green";
+        // score.innerHTML=currentLevelPoints;
+        // latestAnswer.style.borderBottom = "thick solid green";
     } else {
-        latestAnswer.style.borderBottom = "thick solid red";
+        // latestAnswer.style.borderBottom = "thick solid red";
     }
 }
 function generate_image() {
@@ -43,28 +83,28 @@ function generate_image() {
     clearAndReset();
     var numPics = 4;
     //There should be seven/eight levels
-    if (currentLevel >= startingLevel) {
+    if (currentSize >= startingSize) {
         numPics = 1;
         //20/200
-    } else if (currentLevel >= startingLevel-(1*sizeDifference)) {
+    } else if (currentSize >= startingSize-(1*sizeDifference)) {
         numPics = 2;
         //20/100
-    } else if (currentLevel >= startingLevel-(2*sizeDifference)) {
+    } else if (currentSize >= startingSize-(2*sizeDifference)) {
         numPics = 3;
         //20/70
-    } else if (currentLevel >= startingLevel-(3*sizeDifference)) {
+    } else if (currentSize >= startingSize-(3*sizeDifference)) {
         numPics = 4;
         //20/50
-    } else if (currentLevel >= startingLevel-(4*sizeDifference)) {
+    } else if (currentSize >= startingSize-(4*sizeDifference)) {
         numPics = 5;
         //20/40
-    } else if (currentLevel >= startingLevel-(5*sizeDifference)) {
+    } else if (currentSize >= startingSize-(5*sizeDifference)) {
         numPics = 6;
         //20/30
-    } else if (currentLevel >= startingLevel-(6*sizeDifference)) {
+    } else if (currentSize >= startingSize-(6*sizeDifference)) {
         numPics = 7;
         //20/20
-    } else if (currentLevel >= startingLevel-(7*sizeDifference)) {
+    } else if (currentSize >= startingSize-(7*sizeDifference)) {
         numPics = 8;
     }
     for (var i = 0; i < numPics; i++) {
@@ -72,7 +112,7 @@ function generate_image() {
         currentHumanWord = humanWords[currentRand];
         currentLine.push(currentHumanWord);
         //Transform: rotate(180deg)
-        show_image(srces[currentRand], currentLevel, currentLevel, "image", i);
+        show_image(srces[currentRand], currentSize, currentSize, "image", i);
     }
     colorCurrent();
 }
@@ -118,10 +158,12 @@ function show_image(src, width, height, alt, id) {
     tumblingE.height = height;
     tumblingE.alt = alt;
     tumblingE.id = id;
+    tumblingE.paddingRight = "50px";
 
     // This next line will just add it to the <body> tag
     var theDiv = document.getElementById("pictureArea");
     theDiv.appendChild(tumblingE);
+    theDiv.innerHTML += "    ";
     audio.play();
 }
 function colorCurrent() {
@@ -129,7 +171,7 @@ function colorCurrent() {
     for (var i = 0; i<currentLine.length; i++) {
         if (i == currentInspecting) {
             document.getElementById(i).style.borderBottom = "thick solid mistyrose";
-            lookingFor.innerHTML = currentLine[currentInspecting];
+            // lookingFor.innerHTML = currentLine[currentInspecting];
         } else {
             document.getElementById(i).style.border = "none";
         }
@@ -140,7 +182,7 @@ function finishPage() {
     alert("You're done! Final Score = " + totalScore);
 }
 function finishedEntiretyCheck() {
-    if (currentLevel <= 0) {
+    if (currentSize <= 0) {
         finishPage();
         return true;
     } else {
@@ -149,8 +191,8 @@ function finishedEntiretyCheck() {
 }
 function nextLevelCheck() {
     if (currentInspecting+1 >= currentLine.length) {
-        currentLevel = currentLevel - sizeDifference;
-        generate_image(currentLevel);
+        currentSize = currentSize - sizeDifference;
+        generate_image(currentSize);
         return true;
     } else {
         return false;
@@ -167,37 +209,50 @@ function nextLetter() {
         }
     }
 }
-
 function letterCalled(input){
-    console.log("Letter has been called!" + input);
-    if (partOfVocabulary(input)) {
-        displayToUser(input);
-        correctLetterCheck(input);
+    console.log("user has said: " + input);
+    var englishWord;
+    if (englishWord = partOfVocabulary(input)) {
+        displayToUser(englishWord);
+        correctLetterCheck(englishWord);
         nextLetter();
     }
 }
 
+function checkWordIsCorrect() {
+    console.log("It's talking");
+}
+
+function moreThanHalf() {
+
+}
+function passedThisLevel() {
+
+}
+
+
 //Start it all off
 function controllerStart() {
-    console.log("initiating");
-    if (annyang) {
-        var commands = {
-            'left': function() {letterCalled("left");},
-            'right': function() {letterCalled("right");},
-            'up': function() {letterCalled("up");},
-            'down': function() {letterCalled("down");},
-            'it is facing left': function() {letterCalled("left");},
-            'it is facing right': function() {letterCalled("right");},
-            'it is facing up': function() {letterCalled("up");},
-            'it is facing down': function() {letterCalled("down");}
-        };
-        annyang.addCommands(commands);
-        annyang.start({ autoRestart: true, continuous: true });
+    console.log("initiating, setting level to 0");
+    currentLevel = 0;
+    // if (annyang) {
+    //     var commands = {
+    //         'left': function() {letterCalled("left");},
+    //         'right': function() {letterCalled("right");},
+    //         'up': function() {letterCalled("up");},
+    //         'down': function() {letterCalled("down");},
+    //         'it is facing left': function() {letterCalled("left");},
+    //         'it is facing right': function() {letterCalled("right");},
+    //         'it is facing up': function() {letterCalled("up");},
+    //         'it is facing down': function() {letterCalled("down");}
+    //     };
+    //     annyang.addCommands(commands);
+    //     annyang.start({ autoRestart: true, continuous: true });
 
-        generate_image(currentLevel);
+        generate_image(currentSize);
         colorCurrent();
-    } else {
-        console.log("Problem loading Aanyang!");
-    }
+    // } else {
+    //     console.log("Problem loading Aanyang!");
+    // }
 }
 controllerStart();
